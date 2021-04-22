b9ba6427244dfa0f7aaaa474ce1aa9b5008d4af0
Yuriy <tsarenkoyuriidev@gmail.com>
2021-04-22 19:55:12 +0300

    MASTER-2: performed task

--- src/main/java/com/cbs/java_essential/examples/Main.java
+++ src/main/java/com/cbs/java_essential/examples/Main.java
@@ -3,5 +3,7 @@ package com.cbs.java_essential.examples;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
 }
