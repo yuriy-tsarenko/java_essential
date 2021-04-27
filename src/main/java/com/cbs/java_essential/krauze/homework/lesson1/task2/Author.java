5062c138e348c75022bcaf791bb66c0115d6588b
Oleksandr Krauze <o.v.krauze@gmail.com>
2021-04-27 21:13:01 +0300

    HOMEWORK-6: Task 2 performed

---
+++ src/main/java/com/cbs/java_essential/krauze/homework/lesson1/task2/Author.java
@@ -0,0 +1,16 @@
+package com.cbs.java_essential.krauze.homework.lesson1.task2;
+
+public class Author {
+    String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void show(){
+        System.out.println(getAuthor());
+    }
+}
