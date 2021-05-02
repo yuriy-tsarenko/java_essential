a53663fd9ee862290ad6ac843733ecf3e63b75f9
Ruslan Kucherenko <59806506+Svitliy@users.noreply.github.com>
2021-05-02 20:46:32 +0300

    HOMEWORK-4: add file

--- src/main/java/com/cbs/java_essential/ruslan/homework/lesson1/Test.java
+++
@@ -1,4 +0,0 @@
-package com.cbs.java_essential.ruslan.homework.lesson1;
-
-public class Test {
-}
--- src/main/java/com/cbs/java_essential/ruslan/homework/lesson1/task2/Icalculator.java
+++
@@ -1,9 +0,0 @@
-package com.cbs.java_essential.ruslan.homework.lesson1.task2;
-
-/**
- * by Ruslan <svitliy.rk@gmail.com>
- * HomeWork 1 task2
- */
-public interface Icalculator {
-    String calcAreaPerimetr();
-}
