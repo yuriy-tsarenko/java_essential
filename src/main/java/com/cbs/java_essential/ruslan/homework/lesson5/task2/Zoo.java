8cb3907a78165e49852b8a34dcc15e8b132e1ee6
Ruslan Kucherenko <59806506+Svitliy@users.noreply.github.com>
2021-05-18 22:33:26 +0300

    Homework-5: fixed bugs.

--- src/main/java/com/cbs/java_essential/ruslan/homework/lesson5/task2/Zoo.java
+++ src/main/java/com/cbs/java_essential/ruslan/homework/lesson5/task2/Zoo.java
@@ -7,13 +7,16 @@ import java.util.ArrayList;
  * Создать класс Zoo. В классе создать список, в который записать 8 животных через метод add(index, element).
  * Вывести список в консоль.
  */
+
 public class Zoo {
     public static void main(String[] args) {
 
         ArrayList<String> animalList = new ArrayList<>();
 
         for (int i = 0; i < 8; i++) {
-            animalList.add(i, (i + 1) + ". animal " + "index: " + i );
+            animalList.add(i, (i + 1)
+                    + ". animal "
+                    + "index: " + i);
         }
         for (String pet : animalList) {
             System.out.println(pet);
