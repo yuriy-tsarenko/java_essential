a3a17faa91f5ee9de5a8579d92b81fad892fb829
Alexander <return13@ukr.net>
2021-05-24 23:47:12 +0300

    HOMEWORK-7:

---
+++ src/main/java/com/cbs/java_essential/return13/homework/lesson7/Animals.java
@@ -0,0 +1,39 @@
+package com.cbs.java_essential.return13.homework.lesson7;
+
+//Задание 2
+// Создайте перечислительный тип (enum) Animals, содержащий конструктор,
+// который должен принимать целочисленное значение (возраст животного),
+// и содержать перегруженный метод toString(),
+// который должен возвращать название экземпляра и возраст животного.
+
+import com.cbs.java_essential.return13.homework.lesson5.Animal;
+
+public enum Animals {
+    ZUBR(15), DOG(5), CAT(3);
+    private int year;
+
+    Animals(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + ": year = " + year;
+    }
+
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Animals animals = Animals.CAT;
+        Animals animals1 = Animals.DOG;
+        Animals animals2 = Animals.ZUBR;
+        System.out.println(animals.toString());
+        System.out.println(animals1.toString());
+        System.out.println(animals2.toString());
+    }
+}
+
+
+
