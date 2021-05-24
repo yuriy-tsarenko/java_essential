1446f110cdb2713357292954317ab144390c8494
Alexander <return13@ukr.net>
2021-05-24 22:45:11 +0300

    HOMEWORK-4: changes1

--- src/main/java/com/cbs/java_essential/return13/homework/lesson4/Player.java
+++ src/main/java/com/cbs/java_essential/return13/homework/lesson4/Player.java
@@ -2,8 +2,10 @@ package com.cbs.java_essential.return13.homework.lesson4;
 
 //        Задание 3
 //        Создайте 2 интерфейса Playable и Recodable.
-//        В каждом из интерфейсов создайте по 3 метода void play() / void pause() / void stop() и void record() / void pause() / void stop() соответственно.
-//        Создайте производный класс Player от базовых интерфейсов Playable и Recodable. Написать программу, которая выполняет проигрывание и запись.
+//        В каждом из интерфейсов создайте по 3 метода void play() / void pause() / void stop()
+//        и void record() / void pause() / void stop() соответственно.
+//        Создайте производный класс Player от базовых интерфейсов Playable и Recodable.
+//        Написать программу, которая выполняет проигрывание и запись.
 
 
 interface Playable {
