5b8a71291763b06dc4d10e5725eb5978d43ab71c
yuriy tsarenko <61192974+yuriy-tsarenko@users.noreply.github.com>
2021-06-01 14:32:02 +0300

    Feature return13 (#39)
    * Homework return13 lesson2 (#12)
    
    * finisfed some task (#2)
    
    * DEV-5: added lesson1 examples
    
    * created maven build
    
    * DEV-6: performed maven settings, added workflow (mvn build)
    
    * DEV-7: added checkstyle settings
    
    * DEV-8: fixed checkstyle issues
    
    * DEV-9: fixed lombok settings
    
    * DEV-10: added lesson2 examples
    
    * HOMEWORK-1: performed homework
    
    HOMEWORK-2: performed homework
    
    * DEV-11: added lesson3-4 examples
    
    * DEV-12: added lesson5 examples
    
    * DEV-13: added lesson6 examples
    
    * DEV-14: added lesson7 examples
    
    * DEV-15: added lesson8 examples
    
    * HOMEWORK-1:
    
    * HOMEWORK-2: changes2
    
    Co-authored-by: yuriy tsarenko <61192974+yuriy-tsarenko@users.noreply.github.com>
    Co-authored-by: yuriy tsarenko <tsarenkoyuriidev@gmail.com>
    
    * Homework return13 lesson7 (#35)
    
    * HOMEWORK-1: performed homework
    
    HOMEWORK-2: performed homework
    
    * HOMEWORK-3: performed homework
    
    * HOMEWORK-4: performed homework
    
    * HOMEWORK-5: performed homework
    
    * HOMEWORK-2: changes1
    
    * HOMEWORK-7:
    
    * HOMEWORK-7: changes1
    
    * HOMEWORK-7: changes2
    
    * HOMEWORK-7: changes1
    
    * HOMEWORK-7: changes2
    
    * HOMEWORK-1: fixed maven build issues
    
    Co-authored-by: yuriy tsarenko <61192974+yuriy-tsarenko@users.noreply.github.com>
    
    Co-authored-by: return13 <47773511+return13@users.noreply.github.com>

---
+++ src/main/java/com/cbs/java_essential/return13/homework/lesson7/Vehicles.java
@@ -0,0 +1,39 @@
+package com.cbs.java_essential.return13.homework.lesson7;
+
+//Дополнительное задание:
+//Создайте перечислительный тип (enum) Vehicles,
+// содержащий конструктор, который должен принимать целочисленное значение (стоимость автомобиля),
+// содержать метод getColor(), который возвращает строку с цветом автомобиля,
+// и содержать перегруженный метод toString(),
+// который должен возвращать строку с названием экземпляра,
+// цветом и стоимостью автомобиля.
+
+public enum Vehicles {
+    BMW(4000, "white"), AUDI(10000, "black"), VAZ(200, "red");
+    private int price;
+    private String color;
+
+    Vehicles(int price, String color) {
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + " price = " + price + "; color = " + this.getColor();
+    }
+
+
+    public static void main(String[] args) {
+        Vehicles vehicles = Vehicles.BMW;
+        Vehicles vehicles1 = Vehicles.AUDI;
+        Vehicles vehicles2 = Vehicles.VAZ;
+        System.out.println(vehicles);
+        System.out.println(vehicles1);
+        System.out.println(vehicles2);
+    }
+}
