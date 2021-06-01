fb26da5a6230b9eed51d3b9b3a4572e4c29fc9e0
YuraDikalo <79626945+yurii-dikalo@users.noreply.github.com>
2021-06-01 10:12:49 +0300

    Homework dikalo lesson4 (#30)
    * DEV-1: performed task
    
    * DEV-3: performed task2 added some fields
    
    DEV-2: performed task2
    
    * DEV-4: added some files
    
    * DEV-5: added lesson1 examples
    
    * created maven build
    
    * DEV-6: performed maven settings, added workflow (mvn build)
    
    * DEV-7: added checkstyle settings
    
    * DEV-8: fixed checkstyle issues
    
    * DEV-9: fixed lombok settings
    
    * DEV-10: added lesson2 examples
    
    * DEV-11: added lesson3-4 examples
    
    * DEV-12: added lesson5 examples
    
    * DEV-13: added lesson6 examples
    
    * DEV-14: added lesson7 examples
    
    * DEV-15: added lesson8 examples
    
    * DEV-16: added lesson9 examples
    
    * HOMEWORK-LESSON_4: performed tasks
    
    * HOMEWORK-1: fixed maven build issue
    
    Co-authored-by: Yuriy <tsarenkoyuriidev@gmail.com>
    Co-authored-by: yuriy tsarenko <61192974+yuriy-tsarenko@users.noreply.github.com>

---
+++ src/main/java/com/cbs/java_essential/dikalo/homework/lesson4/task1/AbstractHandler.java
@@ -0,0 +1,11 @@
+package com.cbs.java_essential.dikalo.homework.lesson4.task1;
+
+public abstract class AbstractHandler {
+    abstract void open();
+
+    abstract void create();
+
+    abstract void change();
+
+    abstract void save();
+}
