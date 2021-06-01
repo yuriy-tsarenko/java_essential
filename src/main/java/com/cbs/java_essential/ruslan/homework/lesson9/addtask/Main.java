3c779c743418138b19083b4a3ff2e01f8d64ba1c
Ruslan Kucherenko <59806506+Svitliy@users.noreply.github.com>
2021-06-01 11:16:20 +0300

    Homework ruslan lesson9 (#33)
    * rename folder
    
    * write pom.xml
    
    * Merge remote-tracking branch 'origin/feature_ruslan' into feature_ruslan
    
    * HOMEWORK-9: task2,3 start code.
    
    * HOMEWORK-9: Add task and deleted file
    
    * HOMEWORK-9: Add task.
    
    * HOMEWORK-9: All task performed.
    
    * HOMEWORK-9: fix equals.
    
    Co-authored-by: yuriy tsarenko <61192974+yuriy-tsarenko@users.noreply.github.com>

---
+++ src/main/java/com/cbs/java_essential/ruslan/homework/lesson9/addtask/Main.java
@@ -0,0 +1,29 @@
+package com.cbs.java_essential.ruslan.homework.lesson9.addtask;
+
+/*
+by Ruslan svitliy@gmail.com
+Homework9, add task
+Создать класс Animal с именем String, возрастом int, хвостом Boolean.
+В классе переопределить метод toString, что бы вывод был сдедующим «Имя: Васька, возраст: 45, хвост: нет».
+В классе Animal переопределить методы equals & hashCode.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal lion = new Animal("lion", 9, true);
+        Animal cat = new Animal("Cat", 2, true);
+        Animal snail = new Animal("Snail", 1, false);
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(lion);
+        animals.add(cat);
+        animals.add(snail);
+
+            System.out.println(animals);
+        }
+    }
+
